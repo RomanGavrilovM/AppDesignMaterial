f70c20ef7efb0ea79bc27775db51e4a6b0232501
Roman <gavrilov.roma1995@gmail.com>
2022-02-07 04:39:18 +0300

    added wiki search input layout

--- app/src/main/java/com/example/appdesignmaterial/data/App.kt
+++ app/src/main/java/com/example/appdesignmaterial/data/App.kt
@@ -8,12 +8,12 @@ import com.example.appdesignmaterial.domain.repos.NasaPodRepo
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-private const val RETROFIT_BASE_URL = "https://api.nasa.gov/"
+private const val NASA_BASE_URL = "https://api.nasa.gov/"
 
 class App : Application() {
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl(RETROFIT_BASE_URL)
+        .baseUrl(NASA_BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
